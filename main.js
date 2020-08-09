cce4d09ec7e443e3247be8096049c0bb2203e446
Manish Sahani <36563829+sahanimanish@users.noreply.github.com>
2020-08-09 15:38:46 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,9 +3,9 @@ const video = document.getElementById('video')
 const container=document.getElementById('container')
 
 Promise.all([
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+  faceapi.nets.faceRecognitionNet.loadFromUri('./models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('./models'),
+  faceapi.nets.ssdMobilenetv1.loadFromUri('./models')
 ]).then(startVideo)
 
 
